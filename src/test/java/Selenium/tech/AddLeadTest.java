ff2c62ed292835f64fa44516d31f42744ea9a097
Danzee <Danzee@Danzee>
2018-05-01 22:44:38 +0530

    Updating tech project.

---
+++ src/test/java/Selenium/tech/AddLeadTest.java
@@ -0,0 +1,32 @@
+package Selenium.tech;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AddLeadTest {
+  @Test
+  public void f() {
+	  
+	System.setProperty("webdriver.chrome.driver", "artifacts/chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("http://crm.techcanvass.co.in/");
+	String username ="techcanvassuser@techcanvass.co.in", password="user1234";
+	driver.findElement(By.id("btnlogin")).click();
+	driver.findElement(By.name("txtuname")).sendKeys(username);
+	driver.findElement(By.name("txtpwd")).sendKeys(password);
+	driver.findElement(By.id("loginbtn")).click();
+	
+	WebElement table = driver.findElement(By.xpath("//*[@id='ctl00_ContentPlaceHolder1_panalcouncellor']/table[1]"));
+	List<WebElement> th =  table.findElements(By.tagName("th"));
+	for(int i=1; i<th.size();i++)
+		System.out.println("Header "+i+" = "+th.get(i-1).getText());
+	
+	driver.quit();
+  }
+}
